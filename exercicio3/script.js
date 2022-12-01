c5766f6d6803f73d47976646ce4d824c8196ad78
Carol Moreira <caahrolmoreira@gmail.com>
2022-12-01 11:39:35 -0300

    exercicios-II-condicionais

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,8 @@
 //código a ser reescrito
 
+const nome = prompt("Qual é o seu nome?")
+const idade = prompt("Qual sua idade?")
+
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
@@ -12,5 +15,13 @@ if(idade >= 18){
 	console.log("Ainda não pode tirar carteira de motorista");
 }
 
+//If Ternário
 
-console.log("teste 3")
+console.log(nome === "José" ? "Oi, Zé!" : "Olá, " + nome)
+
+console.log(
+	idade >= 18 
+	? "pode tirar carteira de motorista!" 
+	: "Ainda não pode tirar carteira de motorista"
+)
+
